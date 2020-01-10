53171db8b7824f96961a7bf59b57324ab0445cca
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-01-10 18:40:59 +0100

    försökt få live message att fungera i timmar too be continued

--- src/Component/Chat.js
+++ src/Component/Chat.js
@@ -1,15 +1,123 @@
 import React, { Component } from 'react'
+import io from 'socket.io-client'
+
+
+class Chat extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: props.username,
+            messages: [],
+            
+        }
+    }
+
+    
+
+    componentDidMount() {
+        const socket = io('http://3.120.96.16:3000');
+        socket.on('messages', (data) => {
+            
+        this.setState({
+            messages: data
+        })
+    })
+    }
+
+    
+
+
+    // componentDidUpdate() {
+    //     const socket = io('http://3.120.96.16:3000/%27);
+    //     socket.on('new_message', (data) => {
+
+    //     this.setState({
+    //         updatedMessages: data
+    //     })
+    // })
+    // }
+
+    clg = () => {
+        
+    }
+
+    render() {
+        let messages = this.state.messages.map(message => {
+            return <p key={message.id}>{message.username}: {message.content}</p>;
+            
+        });
+
+        let updatedMsg = () => {
+            const socket = io('http://3.120.96.16:3000');
+
+
+            
+            socket.on("new_message", data => {
+                
+                let x = Object.values(data);
+                
+                    x.map(newData => {
+                        console.log(newData)
+                        return <p key={newData.id}>{newData.username}: {newData.content}</p>;
+                    })
+
+            })
+        }
+
+        return (
+            <div className="chat">
+                {messages}
+                
+                {updatedMsg()}
+
+                {/* <h1>new messages</h1>
+                {updatedMessages} */}
+            </div>
+        )
+    }
+}
+
+export default Chat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import React, { Component } from 'react'
 import io from "socket.io-client"
 
 
-export class Chat extends Component {
-    
+class Chat extends Component {
+
+
     render() {
         let username = this.props.username;
         const socket = io("http://3.120.96.16:3000")
-        
+
         return (
             <div>
+                {socket.on('connect', function(){
+                    socket.emit("message", {
+                    username: username,
+                    content: "Hello World",
+                })
+            })};
+
+            {socket.on('new_message', function(data){
+                console.log(data);
+            })}
                 <p>{username}</p>
             </div>
         )
@@ -17,3 +125,5 @@ export class Chat extends Component {
 }
 
 export default Chat
+
+*/
--- src/Component/LogIn.js
+++ src/Component/LogIn.js
@@ -1,3 +1,74 @@
+
+import React, { Component } from 'react'
+import Chat from './Chat'
+
+class LogIn extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            value: ''
+        }
+    }
+
+    setName = e => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    getUsername = () => {
+        console.log(this.state.username);
+    }
+
+    rensaInput = e => {
+        e.preventDefault();
+        this.setState({username: this.state.value, value: ''});
+    }
+
+    openChat = () => {
+        if (this.state.username.length >=1) {
+            return <Chat username={this.state.username}/>
+        }
+        return null;
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.rensaInput.bind(this)}>
+                <input type="text" value={this.state.value} onChange={this.setName.bind(this)}/>
+                <button onClick={this.getUsername.bind(this)}>Submit</button>
+                {this.openChat()}
+            </form>
+        )
+    }
+}
+
+export default LogIn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import React, { Component } from 'react'
 import Chat from './Chat'
 
@@ -31,7 +102,8 @@ class LogIn extends Component {
     }
 
     openChat = () => {
-        if (this.state.username.length >= 1) {
+        if (this.state.username.length >= 3) {
+            console.log(this.state.username);
             return <Chat username={this.state.username}/>
         } else {
             return null
@@ -50,3 +122,5 @@ class LogIn extends Component {
 }
 
 export default LogIn
+
+*/
